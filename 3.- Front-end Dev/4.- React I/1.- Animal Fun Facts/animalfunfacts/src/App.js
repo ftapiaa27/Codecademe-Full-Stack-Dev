e06558a95171182d44774fe9e53da25799d10c97
fausto.tapia <fausto.tapia@toshibagcs.com>
2023-05-17 14:49:44 -0600

    Move react I codes to react apps

--- 3.- Front-end Dev/4.- React I/1.- Animal Fun Facts/animalfunfacts/src/App.js
+++ 3.- Front-end Dev/4.- React I/1.- Animal Fun Facts/animalfunfacts/src/App.js
@@ -1,24 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import { animals } from "./animals";
+import React from "react";
+// import { createRoot } from "react-dom/client";
+
+// const container = document.getElementById("app");
+// const root = createRoot(container);
+const title = "";
+const background = (
+  <img className="background" alt="ocean" src="./images/ocean.jpg" />
+);
+const showBackground = true;
+
+function displayFact(e) {
+  const animal = e.target.alt;
+  const randomIndex = Math.floor(Math.random() * animals[animal].facts.length);
+  const funFact = animals[animal].facts[randomIndex];
+  const p = document.getElementById('fact');
+  p.innerHTML = funFact;
+}
+
+
+const images = [];
+for (const animal in animals) {
+  const image = (
+    <img
+      onClick={displayFact}
+      key={animal}
+      className="animal"
+      alt={animal}
+      src={animals[animal].image}
+      aria-label={animal}
+      role="button"
+    />
+  );
+  images.push(image);
+}
+
+// const animalFacts = (
+//   <div>
+//     <h1>{title || "Click an animal for a fun fact!"}</h1>
+//     {showBackground && background}
+//     <p id='fact'></p>
+//     <div className="animals">{images}</div>
+//   </div>
+// );
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <h1>{title || "Click an animal for a fun fact!"}</h1>
+    {showBackground && background}
+    <p id='fact'></p>
+    <div className="animals">{images}</div>
+  </div>
   );
 }
 
