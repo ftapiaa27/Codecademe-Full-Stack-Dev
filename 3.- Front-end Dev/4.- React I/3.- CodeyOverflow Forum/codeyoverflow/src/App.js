2dbc6fd0bbe269fc0d394c5431a4314b86e8a977
fausto.tapia <fausto.tapia@toshibagcs.com>
2023-05-17 17:04:14 -0600

    Add codeyoverflow app

---
+++ 3.- Front-end Dev/4.- React I/3.- CodeyOverflow Forum/codeyoverflow/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import {comments} from './commentData';
+import Card from './Card';
+
+function App() {
+  return (
+    <div>
+      {
+        comments.map(comment => 
+          <Card commentObject={comment}/>
+        )
+      }
+    </div>
+  );
+
+}
+
+export default App;
