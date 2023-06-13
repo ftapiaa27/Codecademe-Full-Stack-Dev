cb30d8d0f4bd05fcbc4b338da67bd060356be05c
ftapiaa27@gmail.com <Fausto Tapia>
2023-06-13 00:04:47 -0600

    Add last changes to React-Router's first exercise

--- 3.- Front-end Dev/5.- React II/2.- React Router/starting_code/src/components/Profile.js
+++ 3.- Front-end Dev/5.- React II/2.- React Router/starting_code/src/components/Profile.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, Navigate, useNavigate } from "react-router-dom";
 import { selectCurrentUser, selectIsLoggedIn } from "../features/session/sessionSlice";
 
 export default function Profile () {
   const currentUser = useSelector(selectCurrentUser)
   const loggedIn = useSelector(selectIsLoggedIn);
-  
+  const navigate = useNavigate();
   // use loggedIn to return a Navigate
+  if (!loggedIn) {
+    return(
+      <Navigate to='/sign-up'/>
+    );
+    // navigate('/sign-up');
+  }
 
   return (
     <main>
