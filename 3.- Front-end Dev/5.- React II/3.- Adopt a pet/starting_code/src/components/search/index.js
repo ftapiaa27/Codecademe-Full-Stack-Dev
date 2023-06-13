ee21e915e6f219f9ffcce10cd23a260e0db0e594
ftapiaa27@gmail.com <Fausto Tapia>
2023-06-13 09:30:21 -0600

    Add Adopt A Pet project

---
+++ 3.- Front-end Dev/5.- React II/3.- Adopt a pet/starting_code/src/components/search/index.js
@@ -0,0 +1,34 @@
+import React, { useRef } from 'react';
+import { useNavigate, createSearchParams } from 'react-router-dom';
+
+const Search = () => {
+
+  const navigate = useNavigate();
+
+  const searchInputRef = useRef();
+
+  const onSearchHandler = (e) => {
+    e.preventDefault();
+
+    const searchQuery = {
+      name: searchInputRef.current.value
+    }
+
+    // use createSearchParams 
+    const query = createSearchParams(searchQuery);
+
+    // imperatively redirect with useNavigate() returned function
+    navigate(`search?${query}`);
+  };
+
+  return (
+    <form onSubmit={onSearchHandler} className="search-form">
+      <input type="text" className="search" ref={searchInputRef} />
+      <button type="submit" className="search-button">
+        🔎
+      </button>
+    </form>
+  );
+};
+
+export default Search;
